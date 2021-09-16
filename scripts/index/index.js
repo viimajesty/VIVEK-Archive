cfd89a21e032a372bb32b66c089f98b3bf9fbf09
Vivek Kadre <74747996+viveksupport@users.noreply.github.com>
2021-09-16 17:58:30 +0530

    Add files via upload

---
+++ scripts/index/index.js
@@ -0,0 +1,162 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+		
+  gtag('config', 'G-0BFS2QTBQ5');
+
+function invert() {
+var button = document.getElementById('userinput')
+var table = document.getElementsByName("spacing")[0];
+var table1 = document.getElementsByName("spacing")[1];
+var table2 = document.getElementsByName("spacing")[2];
+var table3 = document.getElementsByName("spacing")[3];
+var table4 = document.getElementsByName("spacing")[4];
+var table5 = document.getElementsByName("spacing")[5];
+var table6 = document.getElementsByName("spacing")[6];
+var table7 = document.getElementsByName("spacing")[7];
+var table8 = document.getElementsByName("spacing")[8];
+var table9 = document.getElementsByName("spacing")[9];
+var table10 = document.getElementsByName("spacing")[10];
+var table11 = document.getElementsByName("spacing")[11];
+var table12 = document.getElementsByName("spacing")[12];
+var red = document.getElementsByName("red")[0];
+var red2 = document.getElementsByName("red")[1];
+var y = document.getElementsByTagName("a");
+var i;
+for (i = 19; i < y.length; i++) {
+  y[i].style.color = "#db9bf3" };
+red.style.color = "#fe6035"
+red2.style.color = "#fe6035"
+button.style.background = "#131312";
+button.style.color = "#fefffe";
+button.style.borderColor = "#131312"
+document.body.style.background = '#252b32';
+document.body.style.color = '#fefffe';
+table.style.background = '#131312';
+table1.style.background = '#131312';
+table2.style.background = '#131312';
+table3.style.background = '#131312';
+table4.style.background = '#131312';
+table5.style.background = '#131312';
+table6.style.background = '#131312';
+table7.style.background = '#131312';
+table8.style.background = '#131312';
+table9.style.background = '#131312';
+table10.style.background = '#131312';
+table11.style.background = '#131312';
+table12.style.background = '#131312';
+var button2 = document.getElementById('switch');
+button2.style.background = '#131312';
+button2.style.color = 'white';
+document.getElementById('p1').style.color = "#db9bf3";
+document.getElementById('p2').style.color = "#db9bf3";
+document.getElementById('p3').style.color = "#db9bf3";
+
+
+
+if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+if (window.counter % 2 == 0) { 
+
+  document.cookie="theme;max-age=0";  
+  alert('Please restart your browser for changes to take permanent effect.');
+
+
+
+var button = document.getElementById('userinput')
+var table = document.getElementsByName("spacing")[0];
+var table1 = document.getElementsByName("spacing")[1];
+var table2 = document.getElementsByName("spacing")[2];
+var table3 = document.getElementsByName("spacing")[3];
+var table4 = document.getElementsByName("spacing")[4];
+var table5 = document.getElementsByName("spacing")[5];
+var table6 = document.getElementsByName("spacing")[6];
+var table7 = document.getElementsByName("spacing")[7];
+var table8 = document.getElementsByName("spacing")[8];
+var table9 = document.getElementsByName("spacing")[9];
+var table10 = document.getElementsByName("spacing")[10];
+var table11 = document.getElementsByName("spacing")[11];
+var table12 = document.getElementsByName("spacing")[12];
+var red = document.getElementsByName("red")[0];
+var red2 = document.getElementsByName("red")[1];
+var y = document.getElementsByTagName("a");
+var i;
+for (i = 19; i < y.length; i++) {
+  y[i].style.color = "#0645AD" };
+red.style.color = "#CC0000"
+red2.style.color = "#CC0000"
+button.style.background = "#fefffe";
+button.style.color = "black";
+button.style.borderColor = "#fefffe"
+document.body.style.background = '#e2ecf6';
+document.body.style.color = '#030202';
+table.style.background = '#fefffe';
+table1.style.background = '#fefffe';
+table2.style.background = '#fefffe';
+table3.style.background = '#fefffe';
+table4.style.background = '#fefffe';
+table5.style.background = '#fefffe';
+table6.style.background = '#fefffe';
+table7.style.background = '#fefffe';
+table8.style.background = '#fefffe';
+table9.style.background = '#fefffe';
+table10.style.background = '#fefffe';
+table11.style.background = '#fefffe';
+table12.style.background = '#fefffe';
+document.getElementById('switch').style.background = '#eeefee';
+document.getElementById('p1').style.color = "#0645AD";
+document.getElementById('p2').style.color = "#0645AD";
+document.getElementById('p3').style.color = "#0645AD";
+ }
+ };
+
+}
+function revert() {
+	var el = document.getElementById("invert");
+    el.value = "Light Mode";
+document.cookie = "theme=dark;";
+
+}
+
+
+
+function showdiv() { 
+
+var division = document.getElementById('hidden');
+var sss = document.getElementById('hid');
+if (division.style.display == "none" ) { 
+division.style.display = "block"; sss.style.display = "none";
+} else { division.style.display = "none"; } 
+
+};
+
+
+function change() // no ';' here
+{
+    var elem = document.getElementById("myButton1");
+    if (elem.value=="") elem.value = "Find next";
+	elem.style.width = '90px'; 
+	elem.style.background = 'none';
+};
+
+function darkmode() { 
+var questionmark = document.getElementById("hidden2")
+if (questionmark.style.display == "none") { 
+questionmark.style.display = "block"
+} else { 
+questionmark.style.display = "none" }
+}
+
+function showsubjects() { 
+document.getElementById('onhov').style.display = "block";
+}
+function hidesubjects() { 
+document.getElementById('onhov').style.display = "none"; 
+
+}
+
+function showinfo() { 
+var division = document.getElementById('zip');
+
+if (division.style.display == "none" ) { division.style.display = "block"; } else { division.style.display = "none"; } 
+}
+
---
+++ scripts/index/keyshortcuts.js
@@ -0,0 +1,52 @@
+    $( document ).on( 'keydown', function ( e ) {
+		var form = document.querySelector('form');
+    var aramaAlani = document.getElementById("userinput");
+      if ( e.keyCode === 27 ) {
+        aramaAlani.value = '';
+        aramaAlani.focus();     
+       
+      }
+    }); 
+	$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 48 ) {
+        window.location.replace("#0")      
+      }
+    });
+	$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 49 ) {
+        window.location.replace("#1")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 50 ) {
+        window.location.replace("#2")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 51 ) {
+        window.location.replace("#3")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 52 ) {
+        window.location.replace("#4")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 53 ) {
+        window.location.replace("#5")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 54 ) {
+        window.location.replace("#6")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 55 ) {
+        window.location.replace("#7")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 56 ) {
+        window.location.replace("#8")     
+      }
+    });$( document ).on( 'keydown', function ( e ) {
+      if ( e.keyCode === 57 ) {
+        window.location.replace("#9")     
+      }
+    });
+
